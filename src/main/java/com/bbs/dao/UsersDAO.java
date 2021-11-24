0c093a8e000dbeacd241f72e5131dd454e9c642c
ksg2489 <wwl7936@gmail.com>
2021-11-24 09:35:12 +0900

    First Commit

---
+++ src/main/java/com/bbs/dao/UsersDAO.java
@@ -0,0 +1,6 @@
+package com.bbs.dao;
+
+public interface UsersDAO {
+	
+	public int check_id(String user_id) throws Exception;
+}
---
+++ src/main/java/com/bbs/service/UsersServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bbs.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.bbs.dao.UsersDAO;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+	
+	@Inject
+	UsersDAO dao;
+	
+	@Override
+	public int check_id(String user_id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
