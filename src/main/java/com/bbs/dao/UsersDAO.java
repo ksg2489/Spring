4e9803900d600579c78190c18b8b55f090f57d6f
ksg2489 <wwl7936@gmail.com>
2021-11-25 11:04:58 +0900

    2021-11-25 11:25 commit

--- src/main/java/com/bbs/dao/UsersDAO.java
+++ src/main/java/com/bbs/dao/UsersDAO.java
@@ -2,5 +2,5 @@ package com.bbs.dao;
 
 public interface UsersDAO {
 	
-	public int check_id(String user_id) throws Exception;
+	public int idCheck(String user_id) throws Exception;
 }
--- src/main/java/com/bbs/dao/UsersDAOImpl.java
+++ src/main/java/com/bbs/dao/UsersDAOImpl.java
@@ -1,14 +1,22 @@
 package com.bbs.dao;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UsersDAOImpl implements UsersDAO {
 
+	@Inject
+	SqlSession sqlSession;//루프컨텍스트에서 	
+	
 	@Override
-	public int check_id(String user_id) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+	public int idCheck(String user_id) throws Exception {
+	
+		return sqlSession.selectOne("com.bbs.mappers.bbs.idCheck",user_id);//mappers 에 bbsMapper.xml에 연결해서 가져옴
 	}
+
+
 
 }
